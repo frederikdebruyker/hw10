0974a15a34a4f03646cfc54a0e4b17469c72f415
frederikdebruyker <frederik@debruyker.com>
2019-04-18 18:32:06 -0500

    step 2 finalized

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hw10
 Surfs Up!
+# Step 1 - Climate Analysis and Exploration
+Functionality: climate_starter.ipynb (SQL statements have been commented out as they were converted into ORM)
+
+Images included:
+
+- precipitation.png
+- tobs.png
+- trip avg temp.png
+
+With the exception of the last graph, optional complete.
+
+Original analysis included in climate_starter-initial data analysis.ipynb mainly programmed through sql statements.
+
+
+Run with jupyter notebook.
+
+# Step 2 - Climate App
+Functionality: climate.py
+
+
+Run with python.
--- climate.py
+++ climate.py
@@ -47,8 +47,8 @@ def welcome():
         f"<br><a href=api/precipitation>/api/precipitation</a></br>"
         f"<br><a href=api/stations>/api/stations</a></br>"
         f"<br><a href=api/temperature>/api/temperature</a></br>"
-        f"<br>/api/<<start>start></br>"
-        f"</api/v1.0/justice-league/superhero/batman"
+        f"<br>/api/</br>"
+        
 
     )
 
@@ -118,21 +118,31 @@ def temperature():
     
     return jsonify(temperature_query)
 
-@app.route("/api/", defaults={"2016-10-14"})
 @app.route("/api/<start>")
-def tempvar(start):
-    """the minimum temperature, the average temperature, and the max temperature for a given start date."""
+def calc_temps(start):
+    """the minimum temperature, the average temperature, and the max temperature for a given date range."""
    # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    canonicalized = start.replace(" ", "").lower() #drop spaces
+    sel = [func.min(Measurement.tobs), 
+        func.avg(Measurement.tobs), 
+        func.max(Measurement.tobs)]
+    calc_temp_query = session.query(*sel).filter(Measurement.date >= start).all()
+
+    return jsonify(calc_temp_query)
+
+@app.route("/api/<start>/<end>")
+def calc_temps_range(start, end):
+    """the minimum temperature, the average temperature, and the max temperature for a given date range."""
+   # Create our session (link) from Python to the DB
+    session = Session(engine)
 
     sel = [func.min(Measurement.tobs), 
-           func.avg(Measurement.tobs), 
-           func.max(Measurement.tobs)]
-    tempvar_query = session.query(*sel).filter(Measurement.date >= start, Measurement.date <= start).all()
-
-    return jsonify(tempvar_query)
+        func.avg(Measurement.tobs), 
+        func.max(Measurement.tobs)]
+    calc_temp_range_query = session.query(*sel).filter(Measurement.date >= start, Measurement.date <= end).all()
+        
+    return jsonify(calc_temp_range_query)
 
 
 if __name__ == '__main__':
